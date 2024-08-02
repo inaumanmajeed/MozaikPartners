33dad625dbcd998244c8981b36bd80119b6e8d64
Nauman <inaumanmajeed@gmail.com>
2024-08-02 23:40:36 +0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,34 @@
+const delay = 3000;
+
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * -100;
+
+let current = 0;
+
+function changeSlide(next = true) {
+  if (next) {
+    current = current > maxLeft ? current - 100 : 0;
+  } else {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+
+  slides.style.transform = `translateX(${current}%)`;
+}
+
+let autoChange = setInterval(changeSlide, delay);
+const restart = function () {
+  clearInterval(autoChange);
+  autoChange = setInterval(changeSlide, delay);
+};
+
+// Controls
+document.querySelector(".next-slide").addEventListener("click", function () {
+  changeSlide();
+  restart();
+});
+
+document.querySelector(".prev-slide").addEventListener("click", function () {
+  changeSlide(false);
+  restart();
+});
